d25918560d8e313f116db8dcb691dbdf7445ca91
Codehana <33840240+Codehana@users.noreply.github.com>
2018-02-22 15:38:03 +0100

    Add files via upload

---
+++ ukol_01.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+from pohovor import slovo 
+from pohovor import pismeno
+
+slovo = input("Jaké slovo se má spočítat? ")
+pismeno = input("Jaké písmeno se má spočítat? ")
+
+def spocitej_pismenko(slovo, pismeno):
+	pismeno = pismeno.lower()
+	slovo = slovo.lower()
+	print("Ve slově",slovo," je pismeno",pismeno," {}krát".format(slovo.count(pismeno)))
+	return("")
+print(spocitej_pismenko(slovo, pismeno))
